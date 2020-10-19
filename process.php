fa653c950628658c2e4a0f083dac19705e88dde3
johny2433 <johny.codmw3@gmail.com>
2020-10-19 13:37:49 +0300

    form work update

--- process.php
+++ process.php
@@ -11,5 +11,5 @@
     //send email
     mail($admin_email, "New Form Submission", $message . ' - ' . $phone, "From:" . $email);
     
-    header('Location: http://edgebusiness.github.io/success.html');
+    header('Location: https://johny2433.github.io/edgebusiness/success.html');
   }
